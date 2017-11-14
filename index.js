4f3d1e924ce24cd135d3e7c76c75fe55df06c73d
David Lee <david@Davids-MacBook-Air-2.local>
2017-11-14 15:28:07 -0800

    transfer replit

---
+++ index.js
@@ -0,0 +1,58 @@
+const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    'address': `${searchTerm}`,
+    'key': 'AIzaSyDXPwZNqdJfJyq4uUqixZuWDiL4FigBSVc'
+  }
+  $.getJSON(GOOGLE_MAPS_URL, query, callback);
+}
+
+const WUNDERGROUND_URL = 'https://api.wunderground.com/api/2b3643fa128b66c3/forecast/q';
+
+function getDataFromWunderGroundApi(location, callback) {
+  console.log("wundergroundapi")
+  $.getJSON(`${WUNDERGROUND_URL}/${location.lat},${location.lng}.json`, callback)
+}
+
+function renderResult(result) {
+  return `
+    <div>
+      <p> Day: ${result.title} </p?>
+      <p>${result.fcttext}</p>
+      <p> Chance of rain: ${result.pop}%
+        <a class="js-video-thumbnail" href="${result.icon_url}" target="_blank">
+        <img src="${result.icon_url}">
+        </a>
+      </p>
+      <br></br>
+    </div>
+  `;
+}
+
+function displayWunderGroundData(data) {
+  const results = data.forecast.txt_forecast.forecastday.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+  
+}
+
+function displayGoogleMapsData(data) {
+  // const results = data.items.map((item, index) => renderResult(item));
+  // $('.js-search-results').html(results);
+  console.log(data.results[0].geometry.location);
+  const location = data.results[0].geometry.location;
+  getDataFromWunderGroundApi(location, displayWunderGroundData);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    /*// clear out the input*/
+    queryTarget.val("");
+    getDataFromApi(query, displayGoogleMapsData);
+  });
+}
+
+$(watchSubmit);
