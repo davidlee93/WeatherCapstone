ef8bdc25b55ddde2671cd29508c1f8c30187b5ee
David Lee <david@Davids-MacBook-Air-2.local>
2017-11-15 10:11:01 -0800

    test

--- index.js
+++ index.js
@@ -12,33 +12,21 @@ function getDataFromGeocodeApi(searchTerm, callback) {
   $.getJSON(GOOGLE_GEOCODE_URL, query, callback);
 }
 
-// function getDataFromDirectionsApi(searchTerm, callback) {
-//   const query = {
-//     'origin': `${searchTerm}`,
-//     'destination: '
-//     'key': 'AIzaSyDZ0E2z4VWHcV3YH-Io01lsoORCDsG9jRg'
-//   }
-//   $.getJSON(GOOGLE_DIRECTIONS_URL, query, callback);
-// }
+function getOriginLatLngData(data) {
+  console.log(data.results[0].geometry.location);
+  const originLocation = data.results[0].geometry.location;
+  getDataFromWunderGroundApi(originLocation, displayOriginWunderGroundData);
+}
+
+function getDestinationLatLngData(data) {
+  console.log(data.results[0].geometry.location);
+  const destinationLocation = data.results[0].geometry.location;
+  getDataFromWunderGroundApi(destinationLocation, displayDestinationWunderGroundData);
+}
 
 function getDataFromWunderGroundApi(location, callback) {
   console.log("wundergroundapi")
   $.getJSON(`${WUNDERGROUND_URL}/${location.lat},${location.lng}.json`, callback)
-}
-
-function renderResult(result) {
-  return `
-    <div>
-      <p> Day: ${result.title} </p?>
-      <p>${result.fcttext}</p>
-      <p> Chance of rain: ${result.pop}%
-        <a class="js-video-thumbnail" href="${result.icon_url}" target="_blank">
-        <img src="${result.icon_url}">
-        </a>
-      </p>
-      <br></br>
-    </div>
-  `;
 }
 
 function displayOriginWunderGroundData(data) {
@@ -55,26 +43,40 @@ function displayDestinationWunderGroundData(data) {
   $('.js-search-results').append(destinationResults); 
 }
 
-function getOriginLatLngData(data) {
-  console.log(data.results[0].geometry.location);
-  const originLocation = data.results[0].geometry.location;
-  getDataFromWunderGroundApi(originLocation, displayOriginWunderGroundData);
+function getDataFromDirectionsApi(queryOrigin, queryDestination, callback) {
+  const query = {
+    'origin': `${queryOrigin}`,
+    'destination': `${queryDestination}`,
+    'key': 'AIzaSyDZ0E2z4VWHcV3YH-Io01lsoORCDsG9jRg'
+  }
+  $.getJSON(GOOGLE_DIRECTIONS_URL, query, callback);
 }
 
-function getDestinationLatLngData(data) {
-  console.log(data.results[0].geometry.location);
-  const destinationLocation = data.results[0].geometry.location;
-  getDataFromWunderGroundApi(destinationLocation, displayDestinationWunderGroundData);
+function getDirectionsData(data) {
+  for(let i = 0; i < data.routes.legs[0].steps.length; i++){
+    var directionsList = []
+    console.log(data.routes.legs[0].steps[i].html_instructions);
+    const directionsToLocation = data.routes.legs[0].steps[i].html_instructions;
+    directionsList.push(directionsToLocation);
+  }
+  $('.js-search-results').append(directionsList); 
 }
 
-// function getDirectionsData(data) {
-//   for(let i = 0; i < data.routes.legs[0].steps.length; i++){
-//     console.log(data.routes.legs[0].steps[i].html_instructions);
-//   }
-//   const originLocation = data.results[0].geometry.location;
-//   getDataFromWunderGroundApi(location, displayWunderGroundData);
-// }
 
+function renderResult(result) {
+  return `
+    <div>
+      <p> Day: ${result.title} </p?>
+      <p>${result.fcttext}</p>
+      <p> Chance of rain: ${result.pop}%
+        <a class="js-video-thumbnail" href="${result.icon_url}" target="_blank">
+        <img src="${result.icon_url}">
+        </a>
+      </p>
+      <br></br>
+    </div>
+  `;
+}
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
@@ -89,7 +91,7 @@ function watchSubmit() {
     /*// clear out the input*/
     getDataFromGeocodeApi(queryOrigin, getOriginLatLngData);
     getDataFromGeocodeApi(queryDestination, getDestinationLatLngData);
-
+    getDataFromDirectionsApi(queryOrigin, queryDestination, getDirectionsData);
   });
 }
 
