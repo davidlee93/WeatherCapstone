78bce472a5a3a32fa8acc1346291ec56311de417
David Lee <david@Davids-MacBook-Air-2.local>
2017-11-14 17:56:15 -0800

    fixed functions to display both origin and destination, and the address of both

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const GOOGLE_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
 
-function getDataFromGeocode(searchTerm, callback) {
+const WUNDERGROUND_URL = 'https://api.wunderground.com/api/2b3643fa128b66c3/forecast/q';
+
+const GOOGLE_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/directions/json?';
+
+function getDataFromGeocodeApi(searchTerm, callback) {
   const query = {
     'address': `${searchTerm}`,
     'key': 'AIzaSyDXPwZNqdJfJyq4uUqixZuWDiL4FigBSVc'
@@ -8,7 +12,14 @@ function getDataFromGeocode(searchTerm, callback) {
   $.getJSON(GOOGLE_GEOCODE_URL, query, callback);
 }
 
-const WUNDERGROUND_URL = 'https://api.wunderground.com/api/2b3643fa128b66c3/forecast/q';
+function getDataFromDirectionsApi(searchTerm, callback) {
+  const query = {
+    'origin': `${searchTerm}`,
+    'destination: '
+    'key': 'AIzaSyDZ0E2z4VWHcV3YH-Io01lsoORCDsG9jRg'
+  }
+  $.getJSON(GOOGLE_DIRECTIONS_URL, query, callback);
+}
 
 function getDataFromWunderGroundApi(location, callback) {
   console.log("wundergroundapi")
@@ -30,28 +41,59 @@ function renderResult(result) {
   `;
 }
 
-function displayWunderGroundData(data) {
+function displayOriginWunderGroundData(data) {
   // const results = data.items.map((item, index) => renderResult(item));
   // $('.js-search-results').html(results);
-  const results = data.forecast.txt_forecast.forecastday.map((item, index) => renderResult(item));
-  $('.js-search-results').html(results);
-  
+  $('.js-search-results').append(data.results[0].formatted_address); 
+  const originResults = data.forecast.txt_forecast.forecastday.map((item, index) => renderResult(item));
+  $('.js-search-results').append(originResults); 
 }
 
-function getLatLngData(data) {
+function displayDestinationWunderGroundData(data) {
+  // const results = data.items.map((item, index) => renderResult(item));
+  // $('.js-search-results').html(results);
+   $('.js-search-results').append(data.results[0].formatted_address)
+  const destinationResults = data.forecast.txt_forecast.forecastday.map((item, index) => renderResult(item));
+  $('.js-search-results').append(destinationResults); 
+}
+
+function getOriginLatLngData(data) {
   console.log(data.results[0].geometry.location);
-  const location = data.results[0].geometry.location;
+  const originLocation = data.results[0].geometry.location;
+
+  getDataFromWunderGroundApi(location, displayOriginWunderGroundData);
+}
+
+function getDestinationLatLngData(data) {
+  console.log(data.results[0].geometry.location);
+  const destinationLocation = data.results[0].geometry.location;
+
+  getDataFromWunderGroundApi(location, displayDesitationWunderGroundData);
+}
+
+function getDirectionsData(data) {
+  for(let i = 0; i < data.routes.legs[0].steps.length; i++){
+    console.log(data.routes.legs[0].steps[i].html_instructions);
+  }
+  const originLocation = data.results[0].geometry.location;
   getDataFromWunderGroundApi(location, displayWunderGroundData);
 }
+
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
+    const weatherTarget = $(event.currentTarget).find('.js-destination');
+    const directionsOriginTarget = $(event.currentTarget).find('.js-origin');
+    const queryDestination= weatherTarget.val();
+    const queryOrigin = directionsOriginTarget.val();
+    
+    weatherTarget.val("");
+    directionsOriginTarget.val("");
     /*// clear out the input*/
-    queryTarget.val("");
-    getDataFromGeocode(query, getLatLngData);
+    
+    getDataFromGeocodeApi(queryDestination, getDestinationLatLngData);
+    getDataFromGeocodeApi(queryOrigin, getOriginLatLngData);
   });
 }
 
