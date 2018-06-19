7d9837a7b79d17785cdc74fd1f1091fef5892d5d
David <davidlee93@gmail.com>
2018-06-19 14:45:10 -0700

    cleaning console code

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const GOOGLE_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const GOOGLE_GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
 
-const WUNDERGROUND_URL = 'https://api.wunderground.com/api/2b3643fa128b66c3/forecast/q';
-
-const GOOGLE_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/directions/json?';
+const WUNDERGROUND_URL =
+  "https://api.wunderground.com/api/2b3643fa128b66c3/forecast/q";
 
 function renderRoute(route) {
   const directions = route.steps.map(step => {
-    return (
-      `<li>${step.instructions}</li>`);
-    });
+    return `<li>${step.instructions}</li>`;
+  });
   $("#directions").html(`<ol class="directions"></ol>
     <div class="estimatedTime"></div>
     <div class="distance"></div>`);
@@ -19,63 +17,71 @@ function renderRoute(route) {
 }
 
 function initMap(origin, destination) {
-  var directionsService = new google.maps.DirectionsService;
+  var directionsService = new google.maps.DirectionsService();
   // Optionally create a map
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 37.872899, lng: -122.25854}
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 37.872899, lng: -122.25854 }
   });
   directionsDisplay.setMap(map);
 
-  directionsService.route({
-          origin,
-          destination,
-          travelMode: 'DRIVING'
-  }, function(response, status) {
-      if (status === 'OK') {
-          // Pass data to the map
-          directionsDisplay.setDirections(response);
-
-          // See the data in the console
-          console.log(response.routes[0].legs[0].steps[0].instructions);
-          renderRoute(response.routes[0].legs[0]);
+  directionsService.route(
+    {
+      origin,
+      destination,
+      travelMode: "DRIVING"
+    },
+    function(response, status) {
+      if (status === "OK") {
+        // Pass data to the map
+        directionsDisplay.setDirections(response);
+        renderRoute(response.routes[0].legs[0]);
       } else {
-          window.alert('Directions request failed due to ' + status);
+        window.alert("Directions request failed due to " + status);
       }
-  });
+    }
+  );
 }
 
 function getDataFromGeocodeApi(searchTerm, callback) {
   const query = {
-    'address': `${searchTerm}`,
-    'key': 'AIzaSyDXPwZNqdJfJyq4uUqixZuWDiL4FigBSVc'
+    address: `${searchTerm}`,
+    key: "AIzaSyDXPwZNqdJfJyq4uUqixZuWDiL4FigBSVc"
   };
   $.getJSON(GOOGLE_GEOCODE_URL, query, callback);
 }
 
 function getDestinationLatLngData(data) {
-  console.log(data.results[0].geometry.location);
   const destinationLocation = data.results[0].geometry.location;
-  getDataFromWunderGroundApi(destinationLocation, displayDestinationWunderGroundData);
+  getDataFromWunderGroundApi(
+    destinationLocation,
+    displayDestinationWunderGroundData
+  );
 }
 
 function getDestinationAddress(data) {
-  $('.js-weather-results').html("");
-  $('.weatherforecast').html("");
-  $('.weatherforecast').html(`<h3>Weather Forecast in: ${data.results[0].formatted_address} </h3>`);
+  $(".js-weather-results").html("");
+  $(".weatherforecast").html("");
+  $(".weatherforecast").html(
+    `<h3>Weather Forecast in: ${data.results[0].formatted_address} </h3>`
+  );
 }
 
 function getDataFromWunderGroundApi(location, callback) {
-  console.log("wundergroundapi");
-  $.getJSON(`${WUNDERGROUND_URL}/${location.lat},${location.lng}.json`, callback);
+  $.getJSON(
+    `${WUNDERGROUND_URL}/${location.lat},${location.lng}.json`,
+    callback
+  );
 }
 
 function displayDestinationWunderGroundData(data) {
-  const destinationResults = data.forecast.txt_forecast.forecastday.map((item, index) => renderForecastResult(item));
-  $('.js-weather-results').append(destinationResults);
+  const destinationResults = data.forecast.txt_forecast.forecastday.map(
+    (item, index) => renderForecastResult(item)
+  );
+  $(".js-weather-results").append(destinationResults);
   // hides the last two div elements of forecast - 4 days felt too much/crammed
-  $('.weather-day:nth-last-child(-n+2)').addClass('hidden');
+  $(".weather-day:nth-last-child(-n+2)").addClass("hidden");
 }
 
 function renderForecastResult(result) {
@@ -92,17 +98,19 @@ function renderForecastResult(result) {
 }
 
 function removeHiddenClass() {
-  $('#mapContainer').removeClass('hidden');
-  $('#weatherContainer').removeClass('hidden');
-  $('#wundergroundLogo').removeClass('hidden');
-  document.getElementById('mapContainer').scrollIntoView({behavior: 'smooth'});
+  $("#mapContainer").removeClass("hidden");
+  $("#weatherContainer").removeClass("hidden");
+  $("#wundergroundLogo").removeClass("hidden");
+  document
+    .getElementById("mapContainer")
+    .scrollIntoView({ behavior: "smooth" });
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(event => {
+  $(".js-search-form").submit(event => {
     event.preventDefault();
-    const originTarget = $(event.currentTarget).find('.js-origin');
-    const destinationTarget = $(event.currentTarget).find('.js-destination');
+    const originTarget = $(event.currentTarget).find(".js-origin");
+    const destinationTarget = $(event.currentTarget).find(".js-destination");
     const queryOrigin = originTarget.val();
     const queryDestination = destinationTarget.val();
     /*// clear out the inputs*/
@@ -119,6 +127,4 @@ function watchSubmit() {
   });
 }
 
-
 $(watchSubmit);
-
